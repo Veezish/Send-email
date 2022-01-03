1e4f5eee181e7b48b1d49543b37f6b3034f0d26a
Veezish Ahmad <>
2022-01-03 22:34:52 +0000

    Added a loop to send the same email every 60 seconds.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import yagmail
 import os
+from time import time, sleep
 
-sender = "testm0410@gmail.com"
-receiver = "vdoztlik@sharklasers.com"
+while True:
+  sleep(60 - time()%60)
+  sender = "testm0410@gmail.com"
+  receiver = "vdoztlik@sharklasers.com"
 
-subjects = "LOL"
+  subjects = "LOL"
 
-content = """
-LOL contents
-"""
-yag = yagmail.SMTP(user=sender, password=os.getenv("PASSWORD2"))
-yag.send(to=receiver, subject=subjects, contents=content)
-print("Email sent")
+  content = """
+  LOL contents
+  """
+  yag = yagmail.SMTP(user=sender, password=os.getenv("PASSWORD2"))
+  yag.send(to=receiver, subject=subjects, contents=content)
+  print("Email sent")
