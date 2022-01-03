5dc426a4291a1aa303e1d83e8064b29ff626ebcb
VeezishAhmad2 <>
2022-01-03 22:24:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import yagmail
+import os
+
+sender = "testm0410@gmail.com"
+receiver = "vdoztlik@sharklasers.com"
+
+subjects = "LOL"
+
+content = """
+LOL contents
+"""
+yag = yagmail.SMTP(user=sender, password=os.getenv("PASSWORD2"))
+yag.send(to=receiver, subject=subjects, contents=content)
+print("Email sent")
